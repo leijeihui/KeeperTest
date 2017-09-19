ba56b3daaff4f80d098491530f16a0f0fe7b4dc5
Edmund Louie <leijeihui@gmail.com>
2017-09-19 13:42:42 -0700

    Include addProject to routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,10 @@
 const routes = require('express').Router();
 const userCtrl = require('../db/controllers/userCtrl.js');
+const portfolioCtrl = require('../db/controllers/portfolioCtrl.js');
 
 routes.post('/signin', userCtrl.signin);
 routes.post('/signup', userCtrl.signup);
+routes.post('/addProject', portfolioCtrl.addProject);
 
 module.exports = routes;
 
