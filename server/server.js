388aec9b0984381414bca29fb9645bf0c44c138e
Edmund Louie <leijeihui@gmail.com>
2017-09-19 13:40:27 -0700

    Moved sessions to own file for modularity

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const session = require('express-session');
-const MySQLStore = require('express-mysql-session')(session);
+const sessions = require('./db/sessions.js');
 const multiparty = require('connect-multiparty');
 const routes = require('./routes/routes.js');
 
@@ -10,22 +9,11 @@ const app = express();
 const port = process.env.PORT || 8000;
 const multipartyMiddleware = multiparty();
 
-app.use(require('express-session')({
-  key: 'session',
-  secret: 'SUPER SECRET SECRET',
-  store: new MySQLStore({
-    host: process.env.HOST || 'localhost',
-    user: process.env.USERNAME || 'root', 
-    password: process.env.PASSWORD || '',
-    database: process.env.DATABASE || 'blogger'  
-  })
-})
-);
-
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/../client/public'));
+app.use(sessions);
 app.use('/api', routes);
 
 app.listen(port, () => {
