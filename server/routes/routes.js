adbdeee915541e4301bf2fd90b3650c86cd52c8b
Edmund Louie <leijeihui@gmail.com>
2017-09-21 22:25:19 -0700

    Add s3 route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,10 +1,13 @@
 const routes = require('express').Router();
 const userCtrl = require('../db/controllers/userCtrl.js');
 const portfolioCtrl = require('../db/controllers/portfolioCtrl.js');
+const s3Ctrl = require('../db/controllers/s3Ctrl.js');
+
 
 routes.post('/signin', userCtrl.signin);
 routes.post('/signup', userCtrl.signup);
 routes.post('/addProject', portfolioCtrl.addProject);
+routes.post('/s3', s3Ctrl);
 routes.get('/getProjects', portfolioCtrl.getProjects);
 
 module.exports = routes;
