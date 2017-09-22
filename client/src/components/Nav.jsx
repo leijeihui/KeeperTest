38cc60da64c878b878cc37da81acf194d71555fc
Edmund Louie <leijeihui@gmail.com>
2017-09-21 22:21:00 -0700

    Setup Nav component

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import actions from '../../redux/actions.js';
+import {connect} from 'react-redux';
+import { Link } from 'react-router';
+
+class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+  }
+}
