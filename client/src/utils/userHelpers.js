ab570905c2b49542c4bd975feb7418ac23e29a06
Edmund Louie <leijeihui@gmail.com>
2017-09-25 17:02:53 -0700

    Add user helper functions

---
+++ client/src/utils/userHelpers.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import axios from 'axios';
+
+let handleUserSignup = (userInfo, callback) => {
+  axios.post('/api/signup', userInfo).then(data => {
+    window.location.href = '/#/' + userInfo.username;
+  }).catch(err => {
+    if (err) { console.error(err); }
+  });
+};
+
+export {
+  handleUserSignup
+};
