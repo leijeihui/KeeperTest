ac1d08e82928e54cdbdc3d46a97743c41045f7b1
Edmund Louie <leijeihui@gmail.com>
2017-09-26 12:28:10 -0700

    Add handleUserSignin helper

--- client/src/utils/userHelpers.js
+++ client/src/utils/userHelpers.js
@@ -9,6 +9,15 @@ let handleUserSignup = (userInfo, callback) => {
   });
 };
 
+let handleUserSignin = (userInfo, callback) => {
+  axios.post('/api/signin', userInfo).then(data => {
+    window.location.href = '/#/' + userInfo.username;
+  }).catch(err => {
+    if (err) { console.error(err); }
+  });
+};
+
 export {
-  handleUserSignup
+  handleUserSignup,
+  handleUserSignin
 };
