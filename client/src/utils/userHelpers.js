46fa3e866b53780a934fbbcc7bfbb3d15b9fc06e
Edmund Louie <leijeihui@gmail.com>
2017-10-04 23:40:28 -0700

    Utilize callback function on signin and signup

--- client/src/utils/userHelpers.js
+++ client/src/utils/userHelpers.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 let handleUserSignup = (userInfo, callback) => {
   axios.post('/api/signup', userInfo).then(data => {
     window.location.href = `/#/${userInfo.username}/about`;
+    callback(userInfo.username);
   }).catch(err => {
     if (err) { console.error(err); }
   });
@@ -12,6 +13,7 @@ let handleUserSignup = (userInfo, callback) => {
 let handleUserSignin = (userInfo, callback) => {
   axios.post('/api/signin', userInfo).then(data => {
     window.location.href = `/#/${userInfo.username}/about`;
+    callback(userInfo.username);
   }).catch(err => {
     if (err) { console.error(err); }
   });
