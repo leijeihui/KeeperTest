d1758fd6d62af78f72633f6c47876c55aee53968
Edmund Louie <leijeihui@gmail.com>
2017-10-05 23:00:54 -0700

    Create Nav component

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -2,26 +2,59 @@ import React from 'react';
 import actions from '../../redux/actions.js';
 import {connect} from 'react-redux';
 import { Link } from 'react-router';
+import axios from 'axios';
 
 class Nav extends React.Component {
   constructor(props) {
     super(props);
+
+    this.checkIfLoggedIn = this.checkIfLoggedIn.bind(this);
+    this.state = {};
+    
+  }
+
+  componentDidMount () {
+    this.checkIfLoggedIn();
+  }
+
+  checkIfLoggedIn () {
+    let outer = this;
+    axios.get('/api/isLoggedIn').then(loggedIn => {
+      console.log(loggedIn.data);
+      outer.setState({
+        loggedIn: loggedIn.data
+      });
+    });
   }
 
   render() {
 
     let whichNav = () => {
-      if (this.props.about.length) {
-        return <div></div>;
+      if (!this.state.loggedIn) {
+        return (<div>
+          <form>
+            <input type="text" ref = "search" placeholder="Search User..." /> 
+            <button>Search</button>
+          </form>
+        </div>);
       } else {
-        return <div></div>;
+        return (<div>
+          <form>
+            <input type="text" ref = "search" placeholder="Search User..." /> 
+            <button>Search</button>
+          </form>
+          <a href="#">About</a>
+          <a href="#">Portfolio</a>
+        </div>);
       }
     };
 
     return (
       <div>
-        
+        {whichNav()}
       </div>
     );
   }
 }
+
+export default Nav;
