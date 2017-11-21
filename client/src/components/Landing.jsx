823ec1821515705fb95d862c301fbd596915a8e8
Edmund Louie <leijeihui@gmail.com>
2017-11-20 22:05:01 -0800

    Remove background css

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Modal from './Modal.jsx';
-import css from '../styles/Landing.css';
 
 class Landing extends React.Component {
   constructor(props) {
@@ -10,10 +9,7 @@ class Landing extends React.Component {
   render() {
     return (
       <div className="container">
-        <div className="landing-view">
-          <div className="landing-content">Hello World!</div>
           <Modal />
-        </div>
       </div>
 
     );
